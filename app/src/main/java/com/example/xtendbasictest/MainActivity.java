d2253a39726d7f34a1805c6ab3ab27399f7b2d3e
Hamdi Guerbej <user@xtendplex.com>
2022-01-07 11:33:23 +0100

    improved design + init logic work

--- app/src/main/java/com/example/xtendbasictest/MainActivity.java
+++ app/src/main/java/com/example/xtendbasictest/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.xtendbasictest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+        Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUpBtn = findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Sign Up Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
